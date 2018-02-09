07c4bc69f1827c2898e281df28ccad67c201066b
Eduardo Dias <edu_rodriguesdias@hotmail.com>
2018-02-09 14:03:09 -0200

    Consumo API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,25 +8,22 @@ import { WeatherProvider } from '../../providers/weather/weather';
 })
 export class HomePage {
   weather: any;
+
   location: {
     city:string,
     state:string
   }
 
   constructor(public navCtrl: NavController, private weatherProvider:WeatherProvider) {  
-   	// navigator.geolocation.getCurrentPosition(function(location){
-   	// 	// this.getWeather(location.coords.latitude, location.coords.longitude);
-   	// }); 
   }
-
 
   ionViewWillEnter(){
     this.location = {
-      city: 'Miami',
-      state: 'FL'
+      city: 'Barueri',
+      state: 'SP'
     }
     this.weatherProvider.getWeather(this.location.city, this.location.state).subscribe(weather => {
-        this.weather = weather.current_observation;
+          this.weather = weather.current_observation;
     });
   }
 
